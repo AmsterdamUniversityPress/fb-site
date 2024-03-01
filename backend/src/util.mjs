4f7d06fcb577551160e5eb352b7edccbf9f15dbe
alleycat <git@alleycat.cc>
2024-03-01 13:30:36 +0100

    Update util.mjs

--- backend/src/util.mjs
+++ backend/src/util.mjs
@@ -1,14 +1,16 @@
 import {
   pipe, compose, composeRight,
   map, spreadTo, lets,
-  sprintf1, sprintfN, id, T, nil,
+  sprintf1, sprintfN, id, T, nil, recurry,
+  ifOk, always, die,
 } from 'stick-js/es'
 
 import path from 'path'
 import { fileURLToPath, } from 'url'
 
-import { flatMap, Right, } from 'alleycat-js/es/bilby'
+import { flatMap, Left, Right, } from 'alleycat-js/es/bilby'
 import { composeManyRight, } from 'alleycat-js/es/general'
+import { ifUndefined, } from 'alleycat-js/es/predicate'
 
 import { brightRed, error, } from './io.mjs'
 
@@ -39,3 +41,41 @@ export const env = (key, validate=[null, T], f=id) => {
   )
   return val
 }
+
+export const lookupOn = recurry (2) (
+  o => k => o [k],
+)
+export const lookup = recurry (2) (
+  k => o => lookupOn (o, k),
+)
+
+export const lookupEitherOn = recurry (2) (
+  o => k => o [k] | ifOk (
+    Right, () => Left ("Can't find key " + String (k)),
+  ),
+)
+
+// --- @todo would be nicer to put f at the end.
+// --- @todo versions which take yes/no functions.
+
+export const lookupOnOr = recurry (3) (
+  (f) => (o) => (k) => lookupOn (o, k) | ifUndefined (f, id),
+)
+export const lookupOr = recurry (3) (
+  (f) => (k) => (o) => lookupOnOr (f, o, k),
+)
+export const lookupOnOrV = recurry (3) (
+  (x) => lookupOnOr (x | always),
+)
+export const lookupOrV = recurry (3) (
+  (x) => lookupOr (x | always),
+)
+export const lookupOrDie = recurry (3) (
+  (msg) => (k) => (o) => lookupOnOr (
+    () => die (msg),
+    o, k,
+  )
+)
+export const lookupOnOrDie = recurry (3) (
+  (msg) => (o) => (k) => lookupOrDie (msg, k, o),
+)
