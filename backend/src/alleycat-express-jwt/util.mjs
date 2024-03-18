d6e0f57e67c3996a83fccd73de3508b24aa73c64
alleycat <git@alleycat.cc>
2024-03-18 23:15:25 +0100

    Add `errorToJSON`

--- backend/src/alleycat-express-jwt/util.mjs
+++ backend/src/alleycat-express-jwt/util.mjs
@@ -1,15 +1,23 @@
 import {
   pipe, compose, composeRight,
   dot1, ifPredicate, isString, id,
-  recurry, ifNil, die,
+  recurry, ifNil, die, ok,
 } from 'stick-js/es'
 
 export const noopP = async () => {}
 export const flatMap = dot1 ('flatMap')
 
-// --- note that you can not serialiaze an Error as JSON (for some reason?)
+// --- note that you can not serialize an Error as JSON (for some reason?)
 const ifString = isString | ifPredicate
 export const toJSON = ifString (id, JSON.stringify)
+
+const isError = (e) => ok (e) && ok (e.stack) && ok (e.message)
+const ifError = isError | ifPredicate
+
+export const errorToJSON = toJSON << ifError (
+  (e) => e.message,
+  id,
+)
 
 export const okOrDie = recurry (2) (
   (dieWith) => ifNil (() => die (dieWith), id)
